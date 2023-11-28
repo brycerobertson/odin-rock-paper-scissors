f2868195ea444e3ea65cd5573569dcd8dc77eb62
Bryce <bryce.robertson@protonmail.com>
2023-11-28 16:55:37 +0000

    Created Rock Paper Scissors script
    created the rock paper scissors script, there are two separate functions
    to get computer and user input, the playRound function then compares
    the difference between the choices and returns a value based on a win,
    loss or draw, the playGame function then returns an output string based
    on the return value and increased either player or computer wins, it
    repeats playing rounds until either player or computer wins 3 times.

---
+++ javascript.js
@@ -0,0 +1,59 @@
+
+const choices = ["rock", "paper", "scissors"]
+
+let playerSelection;
+let computerSelection;
+
+let playerWins = 0;
+let computerWins = 0;
+
+function getComputerChoice () {
+    computerSelection = choices[Math.floor(Math.random()*3)];
+}
+
+function getPlayerChoice () {
+    playerSelection = prompt("Choose Rock, Paper or Scissors");
+    playerSelection.toLowerCase();
+}
+
+function playRound () {
+    getComputerChoice();
+    getPlayerChoice();
+
+    let compareChoices = choices.indexOf(playerSelection)-choices.indexOf(computerSelection);
+
+    if (compareChoices === 1 || compareChoices === -2) {
+        return 1;
+    } else if (compareChoices === -1 || compareChoices === 2) {
+        return 2;
+    } else {
+        return 3;
+    }
+}
+
+
+function playGame() {
+    let round = 0;
+    do {
+        let result = playRound();
+        round++;
+        if (result === 1) {
+            console.log(`You win round ${round}! ${playerSelection} beats ${computerSelection}`);
+            playerWins++;
+        } else if (result === 2) {
+            console.log(`You lose round ${round}! ${computerSelection} beats ${playerSelection}`);
+            computerWins++;
+        } else {
+            console.log(`Tie round ${round}! ${computerSelection} ties ${playerSelection}`);
+        }
+    }
+    while (playerWins < 3 && computerWins < 3);
+
+    if (playerWins === 3) {
+        console.log(`You win the game! :D`)
+    } else {
+        console.log(`You lose the game! D:`)
+    }
+}
+
+playGame();
